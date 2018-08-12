9322c3b7f3bee151a26ec096009533af8135063e
jnavb <jorgenavb@gmail.com>
2018-08-12 15:29:56 +0200

    Add list and remove/add funcionality

--- src/pages/tasklist/tasklist.ts
+++ src/pages/tasklist/tasklist.ts
@@ -6,9 +6,31 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'tasklist.html'
 })
 export class TaskListPage {
+	tasks: Array<any> = [];
 
   constructor(public navCtrl: NavController) {
-
+  	this.tasks = [
+  		{title:'Pan', status: 'open'},
+			{title:'Leche', status: 'open'},
+			{title:'Tomate', status: 'open'},
+			{title:'Platanos', status: 'open'}
+  	];
   }
 
+  addItem(){
+  	let theNewTask: string = prompt("New Task");
+  	if (theNewTask !== '') {
+  		this.tasks.push({ title: theNewTask, status: 'open'	})
+  	}
+  }
+  markAsDone(task: any) {
+  	task.status = "done";
+  }
+  removeTaks(task:any) {
+  	task.status = "removed";
+  	let index = this.tasks.indexOf(task);
+  	if (index > -1) {
+  		this.tasks.splice(index, 1);
+  	}
+  }
 }
